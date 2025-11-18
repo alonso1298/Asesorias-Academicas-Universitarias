fab2c6e7b172d73691bc7f2c09c72285c613c61d
alonso1298 <alonsosagrerogranados@gmail.com>
2025-11-17 19:36:31 -0600

    add ConexionDB

---
+++ src/main/java/mx/unam/dgtic/db/ConexionDB.java
@@ -0,0 +1,21 @@
+package mx.unam.dgtic.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionDB {
+    private static final String URL = "jdbc:mysql://localhost:3306/aau_db";
+    private static final String USER = "root";
+    private static final String PASSWORD = "56457977Ac*";
+
+    public static Connection getConnection(){
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        }catch (SQLException e){
+            System.out.println("Error al conectar a la base de datos: " + e.getMessage());
+            return null;
+        }
+    }
+
+}
