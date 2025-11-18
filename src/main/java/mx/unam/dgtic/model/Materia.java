3eb4da5431195e392fcce04aa938360a59ae74de
alonso1298 <alonsosagrerogranados@gmail.com>
2025-11-17 19:37:06 -0600

    add model Materia

---
+++ src/main/java/mx/unam/dgtic/model/Materia.java
@@ -0,0 +1,37 @@
+package mx.unam.dgtic.model;
+
+public class Materia {
+    private Long id;
+    private String nombre;
+    private String descripcion;
+
+    public Materia(Long id, String nombre, String descripcion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    public Materia(String nombre, String descripcion) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    // Getters y Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public String getDescripcion() { return descripcion; }
+    public void setDescripcion(String descripcion) { this.descripcion = descripcion; }
+
+    @Override
+    public String toString() {
+        return "Materia{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                '}';
+    }
+}
