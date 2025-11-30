421f18e42cc0102ba798494094eeb460fa305bf5
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2025-11-30 14:00:16 -0600

    refactor

--- src/main/java/mx/unam/dgtic/app/Main.java
+++ src/main/java/mx/unam/dgtic/app/Main.java
@@ -1,4 +1,4 @@
-package mx.unam.dgtic;
+package mx.unam.dgtic.app;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
