6e90553f19abadb1414226fbdaa139b2b97be7e6
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2025-11-30 22:11:44 -0600

    Add entitie Profesor

---
+++ src/main/java/mx/unam/dgtic/entities/Profesor.java
@@ -0,0 +1,12 @@
+package mx.unam.dgtic.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "profesor")
+public class Profesor {
+
+}
