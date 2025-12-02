1dd4427f90ca351e120d942972cd43c3aa3d9d6b
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2025-12-01 21:05:53 -0600

    add MateriaService

---
+++ src/main/java/mx/unam/dgtic/service/MateriaService.java
@@ -0,0 +1,26 @@
+package mx.unam.dgtic.service;
+
+import mx.unam.dgtic.entities.Materia;
+import mx.unam.dgtic.repository.MateriaRepository;
+
+import java.util.List;
+
+public class MateriaService {
+    private final MateriaRepository repository = new MateriaRepository();
+
+    public List<Materia> listar() {
+        return repository.listar();
+    }
+
+    public void guardar(Materia materia) {
+        repository.guardar(materia);
+    }
+
+    public void editar(Materia materia) {
+        repository.editar(materia);
+    }
+
+    public void eliminar(Long id) {
+        repository.eliminar(id);
+    }
+}
