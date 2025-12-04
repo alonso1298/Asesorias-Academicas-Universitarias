c049500f158079520c4463d89515bde72f1fc23b
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2025-12-03 20:26:42 -0600

    add ProfesorRepository

---
+++ src/main/java/mx/unam/dgtic/repository/ProfesorRepository.java
@@ -0,0 +1,59 @@
+package mx.unam.dgtic.repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import mx.unam.dgtic.entities.Profesor;
+import mx.unam.dgtic.utils.JPAUtil;
+
+import java.util.List;
+
+public class ProfesorRepository {
+
+    public List<Profesor> listar(){
+        EntityManager em = JPAUtil.getEntityManager();
+        return em.createQuery("SELECT p FROM Profesor p", Profesor.class).getResultList();
+    }
+
+    public Profesor buscarPorId(Long id){
+        EntityManager em = JPAUtil.getEntityManager();
+        return em.find(Profesor.class, id);
+    }
+
+    public void guardar(Profesor profesor){
+        EntityManager em = JPAUtil.getEntityManager();
+        em.getTransaction().begin();
+        em.persist(profesor);
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    public void editar(Profesor profesor){
+        EntityManager em = JPAUtil.getEntityManager();
+        em.getTransaction().begin();
+        em.merge(profesor);
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    public void eliminar(Long id){
+        EntityManager em = JPAUtil.getEntityManager();
+        Profesor p = em.find(Profesor.class, id);
+        if (p != null){
+            em.getTransaction().begin();
+            em.remove(p);
+            em.getTransaction().commit();
+        }
+        em.close();
+    }
+
+    // Consulta vanzada de negocio: profesores por experiencia
+    public List<Profesor> buscarPorExperiencia(String exp){
+        EntityManager em = JPAUtil.getEntityManager();
+        TypedQuery<Profesor> q = em.createQuery(
+                "SELECT p FROM Profesor p WHERE p.experiencia LIKE :exp",
+                Profesor.class
+        );
+        q.setParameter("exp", "%" + exp + "%");
+        return q.getResultList();
+    }
+}
