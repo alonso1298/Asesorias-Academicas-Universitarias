8dcf50a008c7d12c5144e524bc4a801c6fee5e21
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2025-12-03 20:58:21 -0600

    add ProfesorService

---
+++ src/main/java/mx/unam/dgtic/service/ProfesorService.java
@@ -0,0 +1,26 @@
+package mx.unam.dgtic.service;
+
+import mx.unam.dgtic.entities.Profesor;
+import mx.unam.dgtic.repository.ProfesorRepository;
+
+import java.util.List;
+
+public class ProfesorService {
+    private final ProfesorRepository rep = new ProfesorRepository();
+
+    public List<Profesor> listar(){
+        return rep.listar();
+    }
+
+    public void guardar(Profesor p){
+        rep.guardar(p);
+    }
+
+    public void editar(Profesor p){
+        rep.editar(p);
+    }
+
+    public void eliminar(Long id){
+        rep.eliminar(id);
+    }
+}
