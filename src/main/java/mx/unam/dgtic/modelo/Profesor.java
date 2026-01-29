add6bf997fb298371cf974eea632b3465ee7e0d0
alonso1298 <alonsosagrerogranados@gmail.com>
2026-01-28 21:12:29 -0600

    Add Profesor

---
+++ src/main/java/mx/unam/dgtic/modelo/Profesor.java
@@ -0,0 +1,34 @@
+package mx.unam.dgtic.modelo;
+
+import java.util.List;
+
+public class Profesor {
+
+    private String  nombre;
+    private Usuario usuario;
+    private List<Materia> materias;
+
+    public Profesor(){}
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public void setMaterias(List<Materia> materias) {
+        this.materias = materias;
+    }
+
+    @Override
+    public String toString() {
+        return "Profesor{" +
+                "nombre='" + nombre + '\'' +
+                ", usuario=" + usuario +
+                ", materias=" + materias +
+                '}';
+    }
+
+}
