1ad381844597b0e8644cfa95b7d5ab1f0714111f
alonso1298 <alonsosagrerogranados@gmail.com>
2026-02-23 20:09:26 -0600

    Practica Modulo 7 - Spring MVC con Thymeleaf

---
+++ src/main/java/mx/unam/aau/AauApplication.java
@@ -0,0 +1,13 @@
+package mx.unam.aau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AauApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/unam/aau/AauApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.unam.aau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
