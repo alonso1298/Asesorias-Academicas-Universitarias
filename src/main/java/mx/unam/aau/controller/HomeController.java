734b0163224485394571d5bda8b16a0cb76a9d33
alonso1298 <alonsosagrerogranados@gmail.com>
2026-02-24 20:56:14 -0600

    add controllers

---
+++ src/main/java/mx/unam/aau/controller/HomeController.java
@@ -0,0 +1,14 @@
+package mx.unam.aau.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String inicio(Model model) {
+        model.addAttribute("contenido", "Bienvenido al Sistema AAU");
+        return "principal";
+    }
+}
