62a29fc36e92c76eb8c48a9df0e6f91912eeb6a7
alonso1298 <alonsosagrerogranados@gmail.com>
2026-03-02 19:40:50 -0600

    add consultas nombrada in repository

--- src/main/java/mx/unam/dgtic/repository/IProfesorRepository.java
+++ src/main/java/mx/unam/dgtic/repository/IProfesorRepository.java
@@ -3,6 +3,8 @@ package mx.unam.dgtic.repository;
 import mx.unam.dgtic.entity.Profesor;
 import mx.unam.dgtic.enums.Especialidad;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -17,5 +19,19 @@ public interface IProfesorRepository extends JpaRepository<Profesor, Integer> {
     List<Profesor> findByNombreAndEspecialidad(String nombre, Especialidad especialidad);
 
     List<Profesor> findByNombreOrExperiencia(String nombre, String experiencia);
+
+    // CONSULTAS NOMBRADAS
+
+    @Query(name = "Profesor.buscarPorEspecialidadJPQL")
+    List<Profesor> buscarPorEspecialidadJPQL(@Param("esp") Especialidad esp);
+
+    @Query(name = "Profesor.buscarPorExperienciaJPQL")
+    List<Profesor> buscarPorExperienciaJPQL(@Param("exp") String exp);
+
+    @Query(name = "Profesor.buscarTodosNative", nativeQuery = true)
+    List<Profesor> buscarTodosNative();
+
+    @Query(name = "Profesor.buscarPorNombreNative", nativeQuery = true)
+    List<Profesor> buscarPorNombreNative(@Param("nombre") String nombre);
 
 }
--- src/main/java/mx/unam/dgtic/repository/IUsuarioRepository.java
+++ src/main/java/mx/unam/dgtic/repository/IUsuarioRepository.java
@@ -3,6 +3,8 @@ package mx.unam.dgtic.repository;
 import mx.unam.dgtic.entity.Usuario;
 import mx.unam.dgtic.enums.Rol;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -21,5 +23,10 @@ public interface IUsuarioRepository extends JpaRepository<Usuario, Integer> {
     boolean existsByEmail(String email);
 
     long countByRol(Rol rol);
+
+    // CONSULTAS NOMBRADAS
+
+    @Query(name = "Usuario.buscarPorRolJPQL")
+    List<Usuario> buscarPorRolJPQL(@Param("rol") Rol rol);
     
 }
