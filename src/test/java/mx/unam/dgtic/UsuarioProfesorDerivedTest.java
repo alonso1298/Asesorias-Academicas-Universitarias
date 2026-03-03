b8d278d0fb052ff8f5abc6805a71b49cff50aba7
alonso1298 <alonsosagrerogranados@gmail.com>
2026-03-02 21:03:50 -0600

    refactor

--- src/test/java/mx/unam/dgtic/UsuarioProfesorDerivedTest.java
+++ src/test/java/mx/unam/dgtic/UsuarioProfesorDerivedTest.java
@@ -9,8 +9,12 @@ import mx.unam.dgtic.repository.IUsuarioRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@Transactional
+@Rollback
 public class UsuarioProfesorDerivedTest {
 
     private static final String ALUMNO = "Alonso Sagrero Granados";
@@ -27,48 +31,59 @@ public class UsuarioProfesorDerivedTest {
         System.out.println(ALUMNO);
 
         // ---------- INSERTAR DATOS ----------
-        Usuario u1 = new Usuario(null,"Carlos","carlos@gmail.com","123", Rol.ADMIN);
-        Usuario u2 = new Usuario(null,"Ana","ana@gmail.com","123", Rol.ALUMNO);
-        Usuario u3 = new Usuario(null,"Luis","luis@gmail.com","123", Rol.ALUMNO);
+        Usuario u1 = new Usuario(null,"Carlos","carlos_test1@gmail.com", "123", Rol.ADMIN);
+        Usuario u2 = new Usuario(null,"Ana","ana_test@gmail.com","123", Rol.ALUMNO);
+        Usuario u3 = new Usuario(null,"Luis","luis_test@gmail.com","123", Rol.ALUMNO);
 
-        usuarioRepository.save(u1);
-        usuarioRepository.save(u2);
-        usuarioRepository.save(u3);
+        u1 = usuarioRepository.save(u1);
+        u2 = usuarioRepository.save(u2);
+        u3 = usuarioRepository.save(u3);
 
-        Profesor p1 = new Profesor(null,"Mario","EMP01","5 años", Especialidad.PROGRAMACION,null);
-        Profesor p2 = new Profesor(null,"Maria","EMP02","10 años",Especialidad.BASES_DE_DATOS,null);
-        Profesor p3 = new Profesor(null,"Marco","EMP03","3 años",Especialidad.PROGRAMACION,null);
+        Profesor p1 = new Profesor(null,"Mario","EMP01","5 años",
+                Especialidad.PROGRAMACION, u1);
+
+        Profesor p2 = new Profesor(null,"Maria","EMP02","10 años",
+                Especialidad.BASES_DE_DATOS, u2);
+
+        Profesor p3 = new Profesor(null,"Marco","EMP03","3 años",
+                Especialidad.PROGRAMACION, u3);
 
         profesorRepository.save(p1);
         profesorRepository.save(p2);
         profesorRepository.save(p3);
 
         // findByNombreContaining
+        System.out.println(ALUMNO);
         System.out.println("\n1️Usuarios que contienen 'a' en el nombre:");
         usuarioRepository.findByNombreContaining("a")
                 .forEach(u -> System.out.println(u.getNombre()));
 
         // existsByEmail
-        System.out.println("\n2️¿Existe ana@gmail.com?");
+        System.out.println(ALUMNO);
+        System.out.println("\n Existe ana@gmail.com?");
         System.out.println(usuarioRepository.existsByEmail("ana@gmail.com"));
 
         // countByRol
-        System.out.println("\n3️Cantidad de usuarios con rol ALUMNO:");
+        System.out.println(ALUMNO);
+        System.out.println("\n Cantidad de usuarios con rol ALUMNO:");
         System.out.println(usuarioRepository.countByRol(Rol.ALUMNO));
 
         // findTop3ByOrderByNombreAsc
-        System.out.println("\n4️Top 3 usuarios ordenados por nombre ASC:");
+        System.out.println(ALUMNO);
+        System.out.println("\n Top 3 usuarios ordenados por nombre ASC:");
         usuarioRepository.findTop3ByOrderByNombreAsc()
                 .forEach(u -> System.out.println(u.getNombre()));
 
         // AND
-        System.out.println("\n5️Profesor por nombre AND especialidad:");
+        System.out.println(ALUMNO);
+        System.out.println("\n Profesor por nombre AND especialidad:");
         profesorRepository
                 .findByNombreAndEspecialidad("Mario", Especialidad.PROGRAMACION)
                 .forEach(p -> System.out.println(p.getNombre()));
 
         // OR
-        System.out.println("\n6️Profesor por nombre OR experiencia:");
+        System.out.println(ALUMNO);
+        System.out.println("\n Profesor por nombre OR experiencia:");
         profesorRepository
                 .findByNombreOrExperiencia("Maria","3 años")
                 .forEach(p -> System.out.println(p.getNombre()));
