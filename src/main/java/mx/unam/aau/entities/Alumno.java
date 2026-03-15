bef64927fbb7a50871c8181876b877f3eb243461
alonso1298 <alonsosagrerogranados@gmail.com>
2026-03-15 11:32:44 -0600

    add Alumno

--- src/main/java/mx/unam/aau/entities/Alumno.java
+++ src/main/java/mx/unam/aau/entities/Alumno.java
@@ -1,4 +1,25 @@
 package mx.unam.aau.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Alumno {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue()
+    private Long idAlumno;
+    private String matricula;
+    private String carrera;
+    private Integer semestre;
+    private String nombre;
+    @Column(name = "usuario_id")
+    private Usuario usuario;
 }
