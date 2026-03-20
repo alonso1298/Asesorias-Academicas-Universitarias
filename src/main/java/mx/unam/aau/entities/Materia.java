9c27dd782d642abb51662a4488d951cf3ece2056
alonso1298 <alonsosagrerogranados@gmail.com>
2026-03-19 21:11:27 -0600

    add entitie profesor

--- src/main/java/mx/unam/aau/entities/Materia.java
+++ src/main/java/mx/unam/aau/entities/Materia.java
@@ -5,8 +5,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Materia {
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/mx/unam/aau/entities/Profesor.java
+++ src/main/java/mx/unam/aau/entities/Profesor.java
@@ -1,4 +1,20 @@
 package mx.unam.aau.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
 public class Profesor {
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idProfesor;
+    private String nombre;
+    private String numeroEmpleado;
+    private String experiencia;
+    @NotNull
+    private String especialidad;
+    private Usuario usuario;
 }
