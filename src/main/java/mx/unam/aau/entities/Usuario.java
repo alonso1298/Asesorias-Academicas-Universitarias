82403fe4a15ba8ee7307c20035c1c273d6ae0b4c
alonso1298 <alonsosagrerogranados@gmail.com>
2026-03-23 21:06:42 -0600

    add enum

--- src/main/java/mx/unam/aau/entities/Usuario.java
+++ src/main/java/mx/unam/aau/entities/Usuario.java
@@ -1,4 +1,15 @@
 package mx.unam.aau.entities;
 
+import jakarta.persistence.Entity;
+import mx.unam.aau.enums.Rol;
+
+@Entity
 public class Usuario {
+
+    private Long id;
+    private String nombre;
+    private String email;
+    private String password;
+    private Rol rol;
+
 }
---
+++ src/main/java/mx/unam/aau/enums/Rol.java
@@ -0,0 +1,7 @@
+package mx.unam.aau.enums;
+
+public enum Rol {
+    ADMIN,
+    PROFESOR,
+    ALUNO
+}
