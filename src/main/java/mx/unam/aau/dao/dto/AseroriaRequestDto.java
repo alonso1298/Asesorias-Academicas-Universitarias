c8865ec41fdf572c9dc7321310ed5b56b203b4ce
alonso1298 <alonsosagrerogranados@gmail.com>
2026-04-05 16:41:35 -0600

    add rquest asesoria dto

---
+++ src/main/java/mx/unam/aau/dao/dto/AseroriaRequestDto.java
@@ -0,0 +1,19 @@
+package mx.unam.aau.dao.dto;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Data
+public class AseroriaRequestDto {
+    private Long alumnoId;
+    private Long profesorId;
+    private Long materiaId;
+
+    private LocalDate fecha;
+    private LocalTime hora;
+
+    private String estado;
+    private String notas;
+}
