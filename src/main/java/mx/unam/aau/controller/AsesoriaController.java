11be0cce3b987cccddd997920e5aaa09936c7f29
alonso1298 <alonsosagrerogranados@gmail.com>
2026-04-05 20:36:41 -0600

    add methods

--- src/main/java/mx/unam/aau/controller/AsesoriaController.java
+++ src/main/java/mx/unam/aau/controller/AsesoriaController.java
@@ -1,13 +1,11 @@
 package mx.unam.aau.controller;
 
-import mx.unam.aau.dao.entities.Asesoria;
-import mx.unam.aau.service.AlumnoService;
+import mx.unam.aau.dao.dto.AsesoriaResponseDto;
 import mx.unam.aau.service.AsesoriaService;
-import mx.unam.aau.service.MateriaService;
-import mx.unam.aau.service.ProfesorService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/asesoria")
@@ -16,62 +14,10 @@ public class AsesoriaController {
     @Autowired
     private AsesoriaService asesoriaService;
 
-    @Autowired
-    private AlumnoService alumnoService;
-
-    @Autowired
-    private ProfesorService profesorService;
-
-    @Autowired
-    private MateriaService materiaService;
-
-    // Lista
+    // GET Listar
     @GetMapping
-    public String listar(Model model) {
-        model.addAttribute("asesorias", asesoriaService.listar());
-        return "paginas/asesorias";
+    public List<AsesoriaResponseDto> listar(){
+        return asesoriaService.listar();
     }
 
-    // Nuevo
-    @GetMapping("/nuevo")
-    public String nuevo(Model model) {
-        model.addAttribute("asesoria", new Asesoria());
-        cargarCatalogos(model);
-        return "paginas/asesoria-form";
-    }
-
-    // Guardar
-    @PostMapping("/guardar")
-    public String guardar(@ModelAttribute Asesoria asesoria, Model model) {
-        try {
-            asesoriaService.guardar(asesoria);
-            return "redirect:/asesorias";
-        } catch (Exception e) {
-            model.addAttribute("error", e.getMessage());
-            cargarCatalogos(model);
-            return "paginas/asesoria-form";
-        }
-    }
-
-    // Editar
-    @GetMapping("/editar/{id}")
-    public String editar(@PathVariable Long id, Model model) {
-        model.addAttribute("asesoria", asesoriaService.buscarPorId(id));
-        cargarCatalogos(model);
-        return "paginas/asesoria-form";
-    }
-
-    // Eliminar
-    @GetMapping("/eliminar/{id}")
-    public String eliminar(@PathVariable Long id) {
-        asesoriaService.eliminar(id);
-        return "redirect:/asesorias";
-    }
-
-    // Metodo auxiliar
-    private void cargarCatalogos(Model model) {
-        model.addAttribute("alumnos", alumnoService.listar());
-        model.addAttribute("profesores", profesorService.listar());
-        model.addAttribute("materias", materiaService.listar());
-    }
 }
