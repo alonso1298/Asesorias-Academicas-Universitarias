7b48cb207c22d6d53ee6fb2be533c10909563dd1
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2026-04-06 21:20:50 -0600

    add service Alumno

--- src/main/java/mx/unam/aau/service/AlumnoService.java
+++ src/main/java/mx/unam/aau/service/AlumnoService.java
@@ -1,7 +1,11 @@
 package mx.unam.aau.service;
 
+import mx.unam.aau.dao.dto.AlumnoRequestDto;
+import mx.unam.aau.dao.dto.AlumnoResponseDto;
 import mx.unam.aau.dao.entities.Alumno;
+import mx.unam.aau.dao.entities.Usuario;
 import mx.unam.aau.dao.repositories.IAlumnoRepository;
+import mx.unam.aau.dao.repositories.IUsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +17,54 @@ public class AlumnoService {
     @Autowired
     private IAlumnoRepository alumnoRepository;
 
-    public List<Alumno> listar(){
-        return alumnoRepository.findAll();
+    @Autowired
+    private IUsuarioRepository usuarioRepository;
+
+    // Listar
+    public List<AlumnoResponseDto> listar(){
+        return alumnoRepository.findAll()
+                .stream()
+                .map(this::convertirADTO)
+                .toList();
+    }
+
+    // Crear
+    public AlumnoResponseDto crear(AlumnoRequestDto alumnoRequestDto){
+        
+        Alumno alumno = convertirAEntidad(alumnoRequestDto);
+        Alumno guardado = alumnoRepository.save(alumno);
+
+        return convertirADTO(guardado);
+    }
+
+    // Entity -> DTO
+    private AlumnoResponseDto convertirADTO(Alumno alumno){
+
+        AlumnoResponseDto dto = new AlumnoResponseDto();
+
+        dto.setId(alumno.getIdAlumno());
+        dto.setMatricula(alumno.getMatricula());
+        dto.setCarrera(alumno.getCarrera());
+        dto.setSemestre(alumno.getSemestre());
+
+        dto.setNombreUsuario(alumno.getUsuario().getNombre());
+        dto.setEmailUsuario(alumno.getUsuario().getEmail());
+
+        return dto;
+    }
+
+    // DTO -> Entity
+    private Alumno convertirAEntidad(AlumnoRequestDto dto){
+
+        Usuario usuario = usuarioRepository.findById(dto.getUsuarioId())
+                .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+
+        Alumno alumno = new Alumno();
+        alumno.setMatricula(dto.getMatricula());
+        alumno.setCarrera(dto.getCarrera());
+        alumno.setSemestre(dto.getSemestre());
+        alumno.setUsuario(usuario);
+
+        return alumno;
     }
 }
