ec7c112f23b703dcb8f876594ba6c2c175f4395d
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2026-04-06 21:45:38 -0600

    add controller Alumno

--- src/main/java/mx/unam/aau/controller/AlumnoController.java
+++ src/main/java/mx/unam/aau/controller/AlumnoController.java
@@ -1,12 +1,11 @@
 package mx.unam.aau.controller;
 
+import mx.unam.aau.dao.dto.AlumnoRequestDto;
 import mx.unam.aau.dao.dto.AlumnoResponseDto;
 import mx.unam.aau.service.AlumnoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,8 +16,28 @@ public class AlumnoController {
     @Autowired
     private AlumnoService alumnoService;
 
+    // Get - Listar
     @GetMapping
     public List<AlumnoResponseDto> listar(){
         return alumnoService.listar();
+    }
+
+    // Get por ID
+    @GetMapping("/{id}")
+    public AlumnoResponseDto obtener(@PathVariable Long id){
+        return alumnoService.buscar(id);
+    }
+
+    // Post - crear
+    @PostMapping
+    public AlumnoResponseDto crear(@RequestBody AlumnoRequestDto alumnoRequestDto){
+        return alumnoService.crear(alumnoRequestDto);
+    }
+
+    // Put - actualizar
+    @PutMapping("/{id}")
+    public AlumnoResponseDto actualizar(@PathVariable Long id,
+                                        @RequestBody AlumnoRequestDto alumnoRequestDto){
+        return alumnoService.actualizar(id, alumnoRequestDto);
     }
 }
