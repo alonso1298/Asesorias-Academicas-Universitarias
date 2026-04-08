7c90c8cfd12f0a4903301bc4ba012c8641c7a04a
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2026-04-07 20:43:19 -0600

    add manejo de errores globales

--- src/main/java/mx/unam/aau/exception/GlobalExceptionHandler.java
+++ src/main/java/mx/unam/aau/exception/GlobalExceptionHandler.java
@@ -1,4 +1,22 @@
 package mx.unam.aau.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    // Manejo general
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> manejarRuntime(RuntimeException ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", ex.getMessage());
+        return error;
+    }
 }
