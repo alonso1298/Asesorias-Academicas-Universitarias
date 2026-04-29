7e675fe9298e9eef9f904f320126a493bcc25a39
alonso1298 <alonsosagrerogranados@gmail.com>
2026-04-28 21:19:21 -0600

    add login and role configuration

--- src/main/java/mx/unam/aau/controller/HomeController.java
+++ src/main/java/mx/unam/aau/controller/HomeController.java
@@ -1,5 +1,6 @@
 package mx.unam.aau.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,5 +11,17 @@ public class HomeController {
     public String inicio(Model model) {
         model.addAttribute("contenido", "Bienvenido al Sistema AAU");
         return "principal";
+    }
+    @GetMapping("/home")
+    public String home(Authentication auth){
+        String rol = auth.getAuthorities().iterator().next().getAuthority();
+        if (rol.equals("ROLE_ADMIN")) {
+            return "redirect:/admin";
+        } else if (rol.equals("ROLE_PROFESOR")){
+            return "redirect:/profesor";
+        } else if (rol.equals("ROLE_ALUMNO")) {
+            return "redirect:/alumno";
+        }
+        return "redirect:/login";
     }
 }
