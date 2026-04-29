8370f1c14a64effba839549e855b16f061d78946
alonso1298 <alonsosagrerogranados@gmail.com>
2026-04-28 20:25:33 -0600

    add method

--- src/main/java/mx/unam/aau/entities/repositories/IUsuarioRepository.java
+++ src/main/java/mx/unam/aau/entities/repositories/IUsuarioRepository.java
@@ -3,5 +3,8 @@ package mx.unam.aau.entities.repositories;
 import mx.unam.aau.entities.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface IUsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByEmail(String email);
 }
