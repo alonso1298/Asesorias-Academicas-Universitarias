64b79c3e9d6873926f871d60c055cd5c0270dfd7
alonso1298 <alonsosagrerogranados@gmail.com>
2026-05-05 20:54:45 -0600

    refactor controller

--- src/main/java/mx/unam/aau/controller/MateriaController.java
+++ src/main/java/mx/unam/aau/controller/MateriaController.java
@@ -1,14 +1,22 @@
 package mx.unam.aau.controller;
 
+import mx.unam.aau.service.MateriaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/materias")
 public class MateriaController {
+
+    @Autowired
+    private MateriaService materiaService;
+
     @GetMapping
-    public String materias(){
+    public String materias(Model model){
+        model.addAttribute("materias", materiaService.listar());
         return "paginas/materias";
     }
 }
