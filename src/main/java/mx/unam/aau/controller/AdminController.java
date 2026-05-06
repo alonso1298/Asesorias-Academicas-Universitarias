8c870057d8f232b9c09b67184426c5c0d1265c28
alonso1298 <alonsosagrerogranados@gmail.com>
2026-05-05 21:45:57 -0600

    add materia-form

--- src/main/java/mx/unam/aau/controller/AdminController.java
+++ src/main/java/mx/unam/aau/controller/AdminController.java
@@ -32,32 +32,4 @@ public class AdminController {
         return "paginas/asesorias";
 
     }
-
-    // Endpoint para crear una nueva materia
-    @GetMapping("/nueva")
-    public String nuevaMateria(Model model){
-        model.addAttribute("materia", new Materia());
-        return "paginas/materia-form";
-    }
-
-    // Endpoint para guardar materia
-    @PostMapping("/guardar")
-    public String guardar(@ModelAttribute Materia materia){
-        materiaService.guardar(materia);
-        return "redirect:/materias";
-    }
-
-    // Editar materia
-    @GetMapping("/editar/{id}")
-    public String editar(@PathVariable Long id, Model model){
-        model.addAttribute("materia", materiaService.buscarPorId(id));
-        return "paginas/materia-form";
-    }
-
-    // Eliminar materia
-    @GetMapping("/eliminar/{id}")
-    public String eliminar(@PathVariable Long id){
-        materiaService.eliminar(id);
-        return "redirect:/materias";
-    }
 }
--- src/main/java/mx/unam/aau/controller/MateriaController.java
+++ src/main/java/mx/unam/aau/controller/MateriaController.java
@@ -1,11 +1,11 @@
 package mx.unam.aau.controller;
 
+import mx.unam.aau.entities.Materia;
 import mx.unam.aau.service.MateriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/materias")
@@ -18,5 +18,33 @@ public class MateriaController {
     public String materias(Model model){
         model.addAttribute("materias", materiaService.listar());
         return "paginas/materias";
+    }
+
+    // Endpoint para crear una nueva materia
+    @GetMapping("/nueva")
+    public String nuevaMateria(Model model){
+        model.addAttribute("materia", new Materia());
+        return "paginas/materia-form";
+    }
+
+    // Endpoint para guardar materia
+    @PostMapping("/guardar")
+    public String guardar(@ModelAttribute Materia materia){
+        materiaService.guardar(materia);
+        return "redirect:/materias";
+    }
+
+    // Editar materia
+    @GetMapping("/editar/{id}")
+    public String editar(@PathVariable Long id, Model model){
+        model.addAttribute("materia", materiaService.buscarPorId(id));
+        return "paginas/materia-form";
+    }
+
+    // Eliminar materia
+    @GetMapping("/eliminar/{id}")
+    public String eliminar(@PathVariable Long id){
+        materiaService.eliminar(id);
+        return "redirect:/materias";
     }
 }
