2e408aeb93a03f7884c7cfa3a079aba9e186125e
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2026-05-11 19:38:00 -0600

    add logger

--- src/main/java/mx/unam/aau/controller/RegistroController.java
+++ src/main/java/mx/unam/aau/controller/RegistroController.java
@@ -1,12 +1,13 @@
 package mx.unam.aau.controller;
 
 import mx.unam.aau.config.PasswordConfig;
-import mx.unam.aau.config.SecurityConfig;
 import mx.unam.aau.entities.Alumno;
 import mx.unam.aau.entities.Usuario;
 import mx.unam.aau.enums.Rol;
 import mx.unam.aau.service.AlumnoService;
 import mx.unam.aau.service.UsuarioService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -24,11 +25,16 @@ public class RegistroController {
     @Autowired
     private PasswordConfig passwordConfig;
 
+    private static final Logger logger =
+            LoggerFactory.getLogger(RegistroController.class);
+
     // Endpoint para registro
     @GetMapping("/registro")
     public String mostrarRegistro(Model model){
+
         Alumno alumno = new Alumno();
         alumno.setUsuario(new Usuario());
+
         model.addAttribute("alumno", alumno);
         return "paginas/registro";
     }
@@ -36,16 +42,23 @@ public class RegistroController {
     @PostMapping("/registro")
     public String registrarAlumno(@ModelAttribute Alumno alumno){
 
-        Usuario usuario = alumno.getUsuario();
-        usuario.setRol(Rol.ALUMNO);
-        usuario.setPassword(
-                passwordConfig.passwordEncoder()
-                        .encode(usuario.getPassword())
-        );
-        usuarioService.guardarUsuario(usuario);
-        alumno.setUsuario(usuario);
-        alumnoService.guardar(alumno);
+        try {
+            Usuario usuario = alumno.getUsuario();
+            usuario.setRol(Rol.ALUMNO);
+            usuario.setPassword(
+                    passwordConfig.passwordEncoder()
+                            .encode(usuario.getPassword())
+            );
+            usuarioService.guardarUsuario(usuario);
+            alumno.setUsuario(usuario);
+            alumnoService.guardar(alumno);
+            logger.info("Alumno registrado correctamente: {}", usuario.getEmail());
 
-        return "redirect:/login";
+            return "redirect:/login";
+
+        } catch (Exception e){
+            logger.error("Error al guardar alumno", e);
+            return "redirect:/registro?error";
+        }
     }
 }
